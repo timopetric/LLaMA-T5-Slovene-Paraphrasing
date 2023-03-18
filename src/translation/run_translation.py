339c7ed412ecd938dced72be663175a75ab9f815
timopetric <timopetric@hotmail.com>
2023-03-18 13:13:49 +0100

    add "en es en" models to back-translation chain, add more test example sentences, improve logs

--- src/translation/run_translation.py
+++ src/translation/run_translation.py
@@ -143,7 +143,7 @@ def initialize():
       _model_platform = "gpu" if _use_gpu_if_available and torch.cuda.is_available() else "cpu"
       _model_path = f"{os.path.dirname(_model_info_path)}/{_model_info['info']['framework'].partition(':')[-1].replace(':','_')}.{_model_info['info']['framework'].partition(':')[0]}"
 
-      model = ModelPT.restore_from(_model_path,map_location="cuda" if _model_platform == "gpu" else "cpu")
+      model = ModelPT.restore_from(_model_path, map_location="cuda" if _model_platform == "gpu" else "cpu")
       model.freeze()
       model.eval()
 
@@ -152,15 +152,24 @@ def initialize():
         del model
         continue
 
-      models[model.src_language.lower()] = {}
+      if model.src_language.lower() not in models:
+        models[model.src_language.lower()] = {}
       models[model.src_language.lower()][model.tgt_language.lower()] = NMTModel(
         tag = _model_tag,
         nemo = model,
         platform = _model_platform,
         active = 0,
       )
-
-  logging.info(f'Loaded models {[ (models[src_lang][tgt_lang].tag,models[src_lang][tgt_lang].platform) for src_lang in models for tgt_lang in models[src_lang] ]}')
+    
+    models_loaded = [
+      (
+        models[src_lang][tgt_lang].tag,
+        models[src_lang][tgt_lang].platform
+      ) 
+      for src_lang in models 
+      for tgt_lang in models[src_lang]
+    ]
+    logging.info(f'Loaded models {models_loaded}')
   logging.info(f'Initialization finished in {time()-time0:.2f}s')
 
   start_time = arrow.utcnow().isoformat()
@@ -169,34 +178,71 @@ def initialize():
 
 
 def main():
-  from collections import namedtuple
   from pprint import pprint
+  from collections import namedtuple
   Item = namedtuple("Item", ["src_language", "tgt_language", "text"])
   
-  repeat_times = 100
+  ########################################
+  # measure translation performance/speed
+  ########################################
+  text_orig = [
+    "Kultni ameriški tehno freak PLASTIKMAN že vrsto let velja za enega izmed najbolj inovativnih in produktivnih ustvarjalcev sodobne elektronske glasbe.",
+    "Prehod iz leta 2004 na 2005 je bil za ameriško igralko Jennifer Aniston izjemno težek, saj je to bilo obdobje, ko se je po desetih letih poslovila od serije Prijatelji, a to ni bilo vse. ",
+    "Na dogodku bodo prisotni predstavniki podjetij, partnerjev fakultete, ki omogočajo, da v sodelovanju z mentorjem iz podjetja (in mentorjem s FRI) pripravite diplomsko nalogo.",
+    "Na dogodku pa boste lahko s podjetji vzpostavili še neposredni stik in se dogovorili za prevzem teme.",
+    "V seštevku norveške turneje je pred nedeljsko zadnjo tekmo v vodstvu Ema Klinec.",
+    "Na sedmi etapi norveške turneje je slavil Anže Lanišek, ki je prehitel vodilnega skakalca zime Halvorja Graneruda.",
+    "V kvalifikacijah za drugo tekmo Lillehammerja je bil s 144 metri najdaljši Timi Zajc, ki pa je bil predvsem vesel pristanka brez padca.",
+    "Dogodek je namenjen predvsem študentom zaključnih letnikov prve stopnje, ki morajo v začetku aprila prijaviti teme diplomskih del.",
+  ]
+  lang_orig = "sl"
+  
+  repeat_times = 1
   duration_all = 0
   text_len_all = 0
-  for _ in trange(repeat_times, desc=f"Translating {repeat_times} texts."):
-    item = Item(
-      src_language="en",
-      tgt_language="sl",
-      text=[
-        "Now it happened that the king of the land held a feast which was to last three days, and out of those who came to it his son was to choose a bride for himself; and Cinderella's two sisters were asked to come. ",
-        "So they called Cinderella, and said, \"Now, comb our hair, brush our shoes, and tie our sashes for us, for we are going to dance at the king's feast.\" ",
-        "Then she did as she was told, but when all was done she could not help crying, for she thought to herself, she would have liked to go to the dance too, and at last she begged her mother very hard to let her go, \"You! Cinderella?\" said she;",
-      ]
-    )
+  for i in trange(repeat_times, desc=f"Translating {repeat_times} texts."):
+    # slo -> en -> es -> en -> slo
+    text_src = text_orig
+
+    if i == repeat_times-1: print("#"*50)
+    if i == repeat_times-1: print(f"{lang_orig:5s}:", text_orig)
+    if i == repeat_times-1: print("-"*50)
+    
+    for lang_src, lang_tgt in [
+          ("sl", "en"),
+          ("en", "es"),
+          ("es", "en"),
+          ("en", "sl"),
+        ]:
+      
+      item = Item(src_language=lang_src, tgt_language=lang_tgt, text=text_src)
+      translated, text_len, duration_secs = translate_text(item)
+      
+      if i == repeat_times-1: print(f"{lang_src}-{lang_tgt}:", translated)
+      if i == repeat_times-1: print("-"*50)
+      text_src = translated
 
     translated, text_len, duration_secs = translate_text(item)
     text_len_all += text_len
     duration_all += duration_secs
-  
-  pprint(translated)
+
+  print("#"*50)
   print(f"{repeat_times} translations took {duration_all:.2f}s or {duration_all/repeat_times:.3f}s per translation.")
   print(f"{repeat_times} translations had  {text_len_all} chars or {text_len_all/repeat_times} average chars.")
   print(f"That is {text_len_all/duration_all:.3f} chars per second.")
   print(f"Or {duration_all/text_len_all:.3f} seconds per char.")
-  
+
+  print("#"*50)
+  print("############### Direct comparison: ###############")
+  for i in range(len(text_orig)):
+    print("ORIGINAL:  ", text_orig[i])
+    print("TRANSLATED:", translated[i])
+    print(f"------------------------------")
+  print("#"*50)
+
+  ############################################
+  # END measure translation performance/speed
+  ############################################
 
 
 if __name__ == "__main__":
