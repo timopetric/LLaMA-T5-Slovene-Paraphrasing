6c7a771bab50253f5c003f3f0886b1319e8592d5
timopetric <timopetric@hotmail.com>
2023-05-23 18:45:41 +0200

    tidy

---
+++ src/llama/processed/check_lines.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# exploratory analysis script to double check that the sentences are
+# aligned between the 3 files (orig-sl, orig-en, tran-sl)
+
+# Set the number of sentences to print
+NUM_SENTENCES=100
+
+# Get the file names from command line arguments
+FILE_1=$1
+FILE_2=$2
+
+# Get the number of lines in the files
+NUM_LINES=$(wc -l < $FILE_1)
+
+# Generate a list of random line numbers
+LINE_NUMBERS=$(shuf -i 1-$NUM_LINES -n $NUM_SENTENCES)
+
+# Loop through the line numbers and print the corresponding sentences
+for LINE_NUMBER in $LINE_NUMBERS; do
+    # Get the sentence from each file
+    SENTENCE_1=$(sed -n "${LINE_NUMBER}p" $FILE_1)
+    SENTENCE_2=$(sed -n "${LINE_NUMBER}p" $FILE_2)
+
+    # Print the sentences
+    echo "#######################################"
+    echo "---------- $FILE_1 -----------"
+    echo "$SENTENCE_1"
+    echo "---------- $FILE_2 -----------"
+    echo "$SENTENCE_2"
+done
--- src/paraphrasing/README.md
+++ src/paraphrasing/README.md
@@ -1,7 +1,24 @@
-change the following:
+
+## config.py
+
+ima več parametrov:
+
+### UČENJE MODELA
 
 DATASET_ORIG_SENTS_FILE = datoteka z izvornimi slovenski stavki od eu tolmača (1 poved na vrstico).
 
 DATASET_TRAN_SENTS_FILE = datoteka s ciljnimi slovenski parafrazami (1 poved na vrstico).
 
 DATASET_PATH = pot kjer se nahajata datoteki DATASET_ORIG_SENTS_FILE in DATASET_TRAN_SENTS_FILE
+
+
+### INFERENCE
+
+OUT_MODEL_CHECKPOINTS_DIR = kje se nahajajo checkpointi naučenih modelov
+
+MODEL_CHECKPOINT_FIN_GLOB = "glob" string, ki pove katere checkpointe naložit in z njimi delati inference
+
+
+#### Potek učenja t5 modelov je na voljo na strani (na voljo za ogled le za našo skupino):
+
+https://wandb.ai/nlp8/huggingface?workspace=user-tp1859
