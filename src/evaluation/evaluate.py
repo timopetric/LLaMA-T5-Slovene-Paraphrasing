81c7cd1cdb2d24f376d1c90622be80fb8dd4e5df
matejkranjec <kranjec.matej@windowslive.com>
2023-05-23 20:19:38 +0200

    batched evaluation

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -2,19 +2,27 @@ from score import score
 import sys
 sys.path.append("../paraphrasing")
 from read_data import euparl
+from tqdm import tqdm
+import transformers
 
+transformers.logging.set_verbosity_error()
 DATA_PATH = "../../data/euparl600k_ensl"
+BATCH_SIZE=64
 diversity_factor = 0.1
 
 data = euparl(path=DATA_PATH, min_length=0, max_numbers=1e10, max_special_characters=1e10,filter_identical=False,shuffle=False)
 n = len(data)
-
 refs = data["original"][:n]
 cands = data["translated"][:n]
-scores = score(cands=cands, refs=refs, model_type="EMBEDDIA/sloberta", use_bleu=True)
-file = open("parascores.out", "w")
-for s in scores:
-    file.write(f"{s}\n")
+file = open("parascores.out", "a")
+iter_range = range(0, len(refs), BATCH_SIZE)
+
+for batch_start in tqdm(iter_range):
+    scores = score(cands=cands[batch_start:batch_start+BATCH_SIZE], refs=refs[batch_start:batch_start+BATCH_SIZE], model_type="EMBEDDIA/sloberta", use_bleu=True, batch_size=BATCH_SIZE)
+    for s in scores:
+        file.write(f"{s}\n")
+
+
 file.close()
 exit(0)
 
