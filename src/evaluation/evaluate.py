04a6b2ea823331b18dcc53938883443891b6ab79
timopetric <timopetric@hotmail.com>
2023-05-24 12:58:54 +0200

    add parascore file genration for both datasets

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -1,52 +1,90 @@
-from score import score
+from score import score as sscore
+import os
 import sys
 sys.path.append("../paraphrasing")
 from read_data import euparl
 from tqdm import tqdm
 import transformers
+import argparse
 
 transformers.logging.set_verbosity_error()
-DATA_PATH = "/d/hpc/home/tp1859/nlp/opus/euparl600k_ensl"
-BATCH_SIZE_CPU=2048*8
-BATCH_SIZE_GPU=1024
-diversity_factor = 0.1
 
-data = euparl(path=DATA_PATH, min_length=0, max_numbers=1e10, max_special_characters=1e10,filter_identical=False,shuffle=False)
-n = len(data)
-refs = data["original"][:n]
-cands = data["translated"][:n]
 
-# clear file
-open("parascores.out", "w").close()
+def main(DATASET_PATH, DATASET_ORIG_SENTS_FILE, DATASET_TRAN_SENTS_FILE):
+    BATCH_SIZE_CPU=2048*8
+    BATCH_SIZE_GPU=1024
+    diversity_factor = 0.1
+    PARASCORES_OUT = os.path.join(DATASET_PATH, "parascores.out")
 
-file = open("parascores.out", "a")
-iter_range = range(0, len(refs), BATCH_SIZE_CPU)
+    print(f"Outputing scores to file: '{PARASCORES_OUT}'")
 
-for batch_start in tqdm(iter_range, desc="CPU batches"):
-    scores = score(
-        cands=cands[batch_start:batch_start+BATCH_SIZE_CPU],
-        refs=refs[batch_start:batch_start+BATCH_SIZE_CPU],
-        model_type="EMBEDDIA/sloberta",
-        use_bleu=True,
-        batch_size=BATCH_SIZE_GPU,
+    data = euparl(
+        path=DATASET_PATH,
+        orig_sl_filename=DATASET_ORIG_SENTS_FILE,
+        tran_sl_filename=DATASET_TRAN_SENTS_FILE,
+        parascore_filename=None,
+        min_length=0,
+        max_numbers=1e10,
+        max_special_characters=1e10,
+        shuffle=False
     )
-    for s in scores:
-        file.write(f"{s}\n")
+    n = len(data)
+    refs = data["original"][:n]
+    cands = data["translated"][:n]
+
+    # clear file
+    open(PARASCORES_OUT, "w").close()
+
+    file = open(PARASCORES_OUT, "a")
+    iter_range = range(0, len(refs), BATCH_SIZE_CPU)
+
+    for batch_start in tqdm(iter_range, desc="CPU batches"):
+        scores = sscore(
+            cands=cands[batch_start:batch_start+BATCH_SIZE_CPU],
+            refs=refs[batch_start:batch_start+BATCH_SIZE_CPU],
+            model_type="EMBEDDIA/sloberta",
+            use_bleu=True,
+            batch_size=BATCH_SIZE_GPU,
+        )
+        for s in scores:
+            file.write(f"{s}\n")
 
 
-file.close()
-exit(0)
+    file.close()
+    exit(0)
 
-results = list(zip(scores, cands, refs))
-results.sort(reverse=True)
-for score, orig, translated in results[:10]:
-    print(score)
-    print(orig)
-    print(translated)
-    print()
-print("#############################################\n")
-for score, orig, translated in results[-10:]:
-    print(score)
-    print(orig)
-    print(translated)
-    print()
+    results = list(zip(scores, cands, refs))
+    results.sort(reverse=True)
+    for score, orig, translated in results[:10]:
+        print(score)
+        print(orig)
+        print(translated)
+        print()
+    print("#############################################\n")
+    for score, orig, translated in results[-10:]:
+        print(score)
+        print(orig)
+        print(translated)
+        print()
+
+if __name__ == "__main__":
+    # DATASET_PATH = "/d/hpc/home/tp1859/nlp/opus/euparl600k_ensl"
+    # DATASET_ORIG_SENTS_FILE = "europarl-orig-sl-all.out"
+    # DATASET_TRAN_SENTS_FILE = "europarl-tran-all.out"
+
+    # DATASET_PATH = "/d/hpc/home/tp1859/nlp/opus/europarl-llama"
+    # DATASET_ORIG_SENTS_FILE = "europarl-orig-sl.out"
+    # DATASET_TRAN_SENTS_FILE = "europarl-llamapara-sl.out"
+    
+    # read args and set vars with defaults
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dataset-path", required=False, default="/d/hpc/home/tp1859/nlp/opus/euparl600k_ensl", help="Path to dataset")
+    ap.add_argument("-o", "--orig-sents-file", required=False, default="europarl-orig-sl-all.out", help="Original sentences file")
+    ap.add_argument("-t", "--tran-sents-file", required=False, default="europarl-tran-all.out", help="Translated sentences file")
+    
+    args = vars(ap.parse_args())
+    DATASET_PATH = args["dataset_path"]
+    DATASET_ORIG_SENTS_FILE = args["orig_sents_file"]
+    DATASET_TRAN_SENTS_FILE = args["tran_sents_file"]
+    
+    main(DATASET_PATH, DATASET_ORIG_SENTS_FILE, DATASET_TRAN_SENTS_FILE)
---
+++ src/evaluation/run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+export TRANSFORMERS_CACHE="/d/hpc/projects/FRI/tp1859/nlp_project8/tmp/transformers_cache"
+export HF_HOME="/d/hpc/projects/FRI/tp1859/nlp_project8/tmp/transformers_cache_models"
+
+DATASET_PATH="/d/hpc/home/tp1859/nlp/opus/europarl-llama"
+DATASET_ORIG_SENTS_FILE="europarl-orig-sl.out"
+DATASET_TRAN_SENTS_FILE="europarl-llamapara-sl.out"
+echo "Evaluating on $DATASET_PATH"
+srun \
+    -N1 \
+    -n1 \
+    --reservation=fri-vr \
+    --cpus-per-task=8 \
+    --partition=gpu \
+    --gpus=1 \
+    --time=2-00:00 \
+    --job-name="nlp" \
+    --mem=64G \
+    singularity exec --nv /d/hpc/projects/FRI/tp1859/nlp_project8/lma/containers/hf.sif python3 evaluate.py \
+        --dataset-path "$DATASET_PATH" \
+        --orig-sents-file "$DATASET_ORIG_SENTS_FILE" \
+        --tran-sents-file "$DATASET_TRAN_SENTS_FILE" \
+    && cp "$DATASET_PATH/parascores.out" parascores-llama.out \
+    && echo "Done."
+
+DATASET_PATH="/d/hpc/home/tp1859/nlp/opus/euparl600k_ensl"
+DATASET_ORIG_SENTS_FILE="europarl-orig-sl-all.out"
+DATASET_TRAN_SENTS_FILE="europarl-tran-all.out"
+echo "Evaluating on $DATASET_PATH"
+srun \
+    -N1 \
+    -n1 \
+    --reservation=fri-vr \
+    --cpus-per-task=8 \
+    --partition=gpu \
+    --gpus=1 \
+    --time=2-00:00 \
+    --job-name="nlp" \
+    --mem=64G \
+    singularity exec --nv /d/hpc/projects/FRI/tp1859/nlp_project8/lma/containers/hf.sif python3 evaluate.py \
+        --dataset-path "$DATASET_PATH" \
+        --orig-sents-file "$DATASET_ORIG_SENTS_FILE" \
+        --tran-sents-file "$DATASET_TRAN_SENTS_FILE" \
+    && cp "$DATASET_PATH/parascores.out" parascores-euparl-tran.out \
+    && echo "Done."
+
+echo "Both parascores.out files should be in the current directory."
+echo "\t parascores-llama.out"
+echo "\t parascores-euparl-tran.out"
