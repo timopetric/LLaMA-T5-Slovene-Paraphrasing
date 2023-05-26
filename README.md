0cf15212e689aef33ba5776399910a890bc97531
domenvilar <domen.vilar@gmail.com>
2023-05-26 15:27:38 +0200

    Update manual_evaluation and add readme.

--- README.md
+++ README.md
@@ -1,12 +1,55 @@
 # Natural language processing course 2022/23: `Paraphrasing sentences`
 
 Team members:
- * `Matej Kranjec`, `63200340`, `mk7972@student.uni-lj.si`
- * `Timotej Petrič`, `63160264`, `tp1859@student.uni-lj.si`
- * `Domen Vilar`, `63180310`, `dv6526@student.uni-lj.si`
+
+- `Matej Kranjec`, `63200340`, `mk7972@student.uni-lj.si`
+- `Timotej Petrič`, `63160264`, `tp1859@student.uni-lj.si`
+- `Domen Vilar`, `63180310`, `dv6526@student.uni-lj.si`
 
 Group public acronym/name: `skupina 8`
- > This value will be used for publishing marks/scores. It will be known only to you and not you colleagues.
+
+> This value will be used for publishing marks/scores. It will be known only to you and not you colleagues.
+
+### Evaluation
+
+#### plot_distribution_scores.py
+
+This script plots the distribution of scores and writes the best paraphrases to a file based on the provided scores and paraphrases directories.
+
+Arguments:
+
+- `--scores_path`: Path to the directory containing the scores files. (Default: "../paraphrasing/paraphrase_scores_t5")
+- `--paraphrases_path`: Path to the directory containing the paraphrases files. (Default: "../paraphrasing/generated_paraphrases_euparl_t5")
+- `--output_directory`: Output directory for the best paraphrases. (Default: "best_generate_paraphrases")
+
+<!-- The script can be run on an HPC system using the following command:
+
+~~~
+srun -n1 -N1 -c8 -G1 --mem=32G --time=1-00:00 --preserve-env --pty --partition=gpu singularity exec --nv /d/hpc/projects/FRI/tp1859/nlp_project8/lma/containers/hf.sif python3 plot_distribution_scores.py --scores_path <scores_directory> --paraphrases_path <paraphrases_directory> --output_directory <output_directory>
+~~~
+
+-->
+
+Output:
+
+- The best paraphrases will be written to a file named "best_paraphrases_euparl_t5.txt" in the specified output directory.
+- Two distribution plots will be generated: "maximum_values.png" and "first_values.png" in the current directory where script will run.
+
+#### manual_evaluation.py
+
+This script is used for manual evaluation of the best paraphrases.
+
+Arguments:
+
+The script accepts the following arguments:
+
+- `--file1_path`: Path to the first file containing paraphrases.
+- `--file2_path`: Path to the second file containing paraphrases.
+- `--file_original_path`: Path to the file containing original sentences.
+- `--scores_folder_path`: Path to the folder where the evaluation scores will be stored.
+
+Note: If the specified `scores_folder_path` does not exist, the script will create it.
 
 ### Report
+
 Report is located in folder `report`
--- src/manual_evaluation/manual_evaluation.py
+++ src/manual_evaluation/manual_evaluation.py
@@ -1,6 +1,7 @@
 import random
 import datetime
 import os
+import argparse
 
 task = ["grammar", "lexical_divergence", "meaning_preservation", "fluency"]
 
@@ -23,86 +24,91 @@ def get_user_scores(sentence):
         score = input(task[i] + "(1-5): ")
         scores.append(score)
     return scores
-
-# file1_path = "/home/domen/Documents/Faks/NLP/Manual Evaluation/mocacu/best_paraphrases_mocacu_t5.txt"
-# file2_path = "/home/domen/Documents/Faks/NLP/Manual Evaluation/mocacu/4MaCoCu-1000_llamapara_en_to_sl_tran_from3.out"
-
-file1_path = "mocacu/best_paraphrases_mocacu_t5.txt"
-file2_path = "mocacu/4MaCoCu-1000_llamapara_en_to_sl_tran_from3.out"
-
-# file_original_path = "/home/domen/Documents/Faks/NLP/Manual Evaluation/mocacu/1mocacu_200x_orig_sl.txt"
-file_original_path = "mocacu/1mocacu_200x_orig_sl.txt"
-
-scores_folder_path = "scores"
-
-current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M")
-
-# file1_scores_path = f"/home/domen/Documents/Faks/NLP/Manual Evaluation/scores/scores_t5_{current_datetime}.txt"
-# file2_scores_path = f"/home/domen/Documents/Faks/NLP/Manual Evaluation/scores/scores_vicuna_{current_datetime}.txt"
-
-file1_scores_path = os.path.join(scores_folder_path, f"scores_t5_{current_datetime}.txt")
-file2_scores_path = os.path.join(scores_folder_path, f"scores_vicuna_{current_datetime}.txt")
-
-if not os.path.exists(scores_folder_path):
-    os.makedirs(scores_folder_path)
-
-#     open(file_original_path, "r") as file_original, \
-
-with open(file1_path, "r") as file1, open(file2_path, "r") as file2, \
-     open(file1_scores_path, "w") as file1_scores, \
-     open(file_original_path, "r") as file_original, \
-     open(file2_scores_path, "w") as file2_scores:
-    
-    file1_scores.write(",".join(task) + "\n")
-    file2_scores.write(",".join(task) + "\n")
-
-    user = input("Enter a, b or c to start the evaluation: ")
-
-    if (user == "a"):
-        start_line = 0  # Define the starting line
-        end_line = 30  # Define the ending line
-    elif (user == "b"):
-        start_line = 30
-        end_line = 60
-    elif (user == "c"):
-        start_line = 60
-        end_line = 90
     
 
-    for _ in range(start_line):
-        file_original.readline()
-        file1.readline()
-        file2.readline()
+def main(file1_path, file2_path, file_original_path, scores_folder_path):
 
+    # file1_path = "mocacu/best_paraphrases_mocacu_t5.txt"
+    # file2_path = "mocacu/4MaCoCu-1000_llamapara_en_to_sl_tran_from3.out"
 
-    #for line1, line2, line_original in zip(file1, file2, file_original):
-    #for line1, line2 in zip(file1, file2):
-    for _ in range(end_line - start_line):
-        # Get a random line from the files
-        #
-        line_original = file_original.readline()
-        line1 = file1.readline()
-        line2 = file2.readline()
-        print("Original sentence: ", line_original)
-        print("-----------------------------------")
+    # file_original_path = "mocacu/1mocacu_200x_orig_sl.txt"
+
+    # scores_folder_path = "scores"
+
+    current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M")
+
+    file1_scores_path = os.path.join(scores_folder_path, f"scores_t5_{current_datetime}.txt")
+    file2_scores_path = os.path.join(scores_folder_path, f"scores_vicuna_{current_datetime}.txt")
+
+    if not os.path.exists(scores_folder_path):
+        os.makedirs(scores_folder_path)
+
+    #     open(file_original_path, "r") as file_original, \
+
+    with open(file1_path, "r") as file1, open(file2_path, "r") as file2, \
+        open(file1_scores_path, "w") as file1_scores, \
+        open(file_original_path, "r") as file_original, \
+        open(file2_scores_path, "w") as file2_scores:
         
-        if random.choice([True, False]):
-            sentence1 = line1
-            sentence2 = line2
-            file_indicator = 1
-        else:
-            sentence1 = line2
-            sentence2 = line1
-            file_indicator = 2
-        
-        scores1 = get_user_scores(sentence1)
-        scores2 = get_user_scores(sentence2)
+        file1_scores.write(",".join(task) + "\n")
+        file2_scores.write(",".join(task) + "\n")
 
-        
-        if file_indicator == 1:
-            file1_scores.write( ",".join(scores1) + "\n")
-            file2_scores.write(",".join(scores2) + "\n")
-        else:
-            file1_scores.write(",".join(scores2) + "\n")
-            file2_scores.write(",".join(scores1) + "\n")
+        user = input("Enter a, b or c to start the evaluation: ")
 
+        if (user == "a"):
+            start_line = 0  # Define the starting line
+            end_line = 30  # Define the ending line
+        elif (user == "b"):
+            start_line = 30
+            end_line = 60
+        elif (user == "c"):
+            start_line = 60
+            end_line = 90
+        
+
+        for _ in range(start_line):
+            file_original.readline()
+            file1.readline()
+            file2.readline()
+
+
+        #for line1, line2, line_original in zip(file1, file2, file_original):
+        #for line1, line2 in zip(file1, file2):
+        for _ in range(end_line - start_line):
+            # Get a random line from the files
+            #
+            line_original = file_original.readline()
+            line1 = file1.readline()
+            line2 = file2.readline()
+            print("Original sentence: ", line_original)
+            print("-----------------------------------")
+            
+            if random.choice([True, False]):
+                sentence1 = line1
+                sentence2 = line2
+                file_indicator = 1
+            else:
+                sentence1 = line2
+                sentence2 = line1
+                file_indicator = 2
+            
+            scores1 = get_user_scores(sentence1)
+            scores2 = get_user_scores(sentence2)
+
+            
+            if file_indicator == 1:
+                file1_scores.write( ",".join(scores1) + "\n")
+                file2_scores.write(",".join(scores2) + "\n")
+            else:
+                file1_scores.write(",".join(scores2) + "\n")
+                file2_scores.write(",".join(scores1) + "\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluation Script")
+    parser.add_argument("--file1_path", type=str, default="mocacu/best_paraphrases_mocacu_t5.txt", help="Path to file 1")
+    parser.add_argument("--file2_path", type=str, default="mocacu/4MaCoCu-1000_llamapara_en_to_sl_tran_from3.out", help="Path to file 2")
+    parser.add_argument("--file_original_path", type=str, default="mocacu/1mocacu_200x_orig_sl.txt", help="Path to the original file")
+    parser.add_argument("--scores_folder_path", type=str, default="scores", help="Path to the folder to store scores")
+    args = parser.parse_args()
+
+    main(args.file1_path, args.file2_path, args.file_original_path, args.scores_folder_path)
